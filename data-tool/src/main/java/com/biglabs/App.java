5aa0ee28d254de5dd9fe1cb4babc3ea510e17446
Truc Thai <truc.thai-nguyen@hp.com>
2016-05-28 00:04:45 +0700

    - init tool to publish data to kafka

---
+++ data-tool/src/main/java/com/biglabs/App.java
@@ -0,0 +1,78 @@
+package com.biglabs;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.err.println("Usage: data-tool <brokers> <topics>\n" +
+                    "  <brokers> is a list of one or more Kafka brokers\n" +
+                    "  <topics> is a list of one or more kafka topics to publish to\n\n");
+            System.exit(1);
+        }
+
+        String brokers = args[0];
+        String topics = args[1];
+        String dataRoot = args[2];
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", brokers);
+//        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        ArrayList<File> files = new ArrayList<File>();
+        listFiles(dataRoot, files);
+
+        if(files.size() == 0){
+            System.err.println("The data directory doesn't contain any file");
+            System.exit(2);
+        }
+
+        for(File file: files){
+            try{
+                Reader reader = new FileReader(file.getAbsolutePath());
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line = bufferedReader.readLine();
+                while(line != null){
+                    producer.send(new ProducerRecord<String, String>(topics, file.getName(), line));
+                    line = bufferedReader.readLine();
+                }
+            } catch (Exception ex){
+                System.err.println(ex.toString());
+            }
+        }
+    }
+
+    public static void listFiles(String dir, ArrayList<File> files){
+        File dirFile = new File(dir);
+        if(dirFile.isDirectory()){
+            File[] fileList = dirFile.listFiles();
+            for(File file: fileList){
+                if(file.isFile()){
+                    files.add(file);
+                } else if(file.isDirectory()){
+                    listFiles(file.getAbsolutePath(), files);
+                }
+            }
+        } else if(dirFile.isFile()){
+            files.add(dirFile);
+        }
+
+    }
+}

c924e96066c2647ce9cc3f715df0781b4bebd152
Tai Lay <layatai@gmail.com>
2016-06-11 21:17:07 +0700

    Tai Lay - add mem

--- bin/submit.sh
+++ bin/submit.sh
@@ -1,2 +1,1 @@
-mvn install
-/opt/iot/spark-1.6.1-bin-hadoop2.6/bin/spark-submit  --class com.biglabs.apocalypse.timeseries.PowerKafkaStreaming --master spark://192.168.1.131:7077 ./time-series/target/timeseries-1.0.0-SNAPSHOT-jar-with-dependencies.jar
+/opt/iot/master-spark/spark-1.6-bin-custom-spark/bin/spark-submit  --class com.biglabs.apocalypse.timeseries.PowerKafkaStreaming --master spark://192.168.1.131:7077 ./time-series/target/timeseries-1.0.0-SNAPSHOT-jar-with-dependencies.jar
